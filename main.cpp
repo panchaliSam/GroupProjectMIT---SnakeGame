eccc392327b30c81eee3ef3e4850230eccbaff43
Panchali2003 <122316166+Panchali2003@users.noreply.github.com>
2023-09-05 16:11:56 +0530

    ImplemetedSetUp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,48 @@
 #include<iostream>
  using namespace std;
+
+ bool gameOver;
+ const int width = 20;
+ const int height = 20;
+ int x;
+ int y;
+ int fruitX;
+ int fruitY;
+ int score;
+
+enum eDirection{STOP = 0, LEFT, RIGHT, UP, DOWN};
+eDirection dir;
+
+ void Setup(){
+    gameOver = false;
+    dir = STOP;
+    x = width / 2;
+    y = height / 2;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+    score = 0;
+ }
+
+ void Draw(){
+
+ }
+
+ void Input(){
+
+ }
+
+ void Logic(){
+
+ }
+
  int main(){
 
-    cout << "Hello World!";
+    Setup();
+    while (!gameOver){
+        Draw();
+        Input();
+        Logic();
+        //Sleep(10);
+     }
     return 0;
  }

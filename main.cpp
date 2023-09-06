a8bb024847e0536a99f37d1bdb3db1c1b17e44fe
Panchali2003 <122316166+Panchali2003@users.noreply.github.com>
2023-09-06 12:19:47 +0530

    completed

--- main.cpp
+++ main.cpp
@@ -1,152 +1,215 @@
-#include <iostream>
-#include <conio.h>
-#include <windows.h>
+#include<iostream>
+#include<windows.h>
+#include<conio.h>
 using namespace std;
+
 bool gameOver;
-const int width = 20;
-const int height = 20;
-int x, y, fruitX, fruitY, score;
-int tailX[100], tailY[100];
+const int width = 20;//To set the width of the wall
+const  int height = 20;//To seth the height of the wall
+
+int x;
+int y;
+int fruitX;
+int fruitY;
+
+int score;
+
+enum eDirection{STOP = 0, LEFT, RIGHT, UP, DOWN};//This helps to move the snake up,down,left,right
+eDirection dir;//Hold the direction of the snake
+
+//x coordinate of the tail
+int xTail[100];
+//y coordunate of the tail
+int yTail[100];
+//Length of the tail 
 int nTail;
-enum eDirecton { STOP = 0, LEFT, RIGHT, UP, DOWN};
-eDirecton dir;
-void Setup()
-{
-    gameOver = false;
-    dir = STOP;
-    x = width / 2;
-    y = height / 2;
-    fruitX = rand() % width;
-    fruitY = rand() % height;
-    score = 0;
+
+void Setup(){	
+	gameOver = false;
+	dir = STOP;//The snake will not move until we start the moving it
+	
+	//To set the snake head at the middle of the border
+	x = width / 2;
+	y = height /2;
+	
+	//Here the fruit will be placed in a random place
+	fruitX = rand() % width;//rand() generates a random number 0 to (width - 1)
+	fruitY = rand() % height;
+	
+	//Set the score as 0 at the begining of the game
+	score = 0;
 }
-void Draw()
-{
-    system("cls"); //system("clear");
-    for (int i = 0; i < width+2; i++)
-        cout << "#";
-    cout << endl;
- 
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            if (j == 0)
-                cout << "#";
-            if (i == y && j == x)
-                cout << "O";
-            else if (i == fruitY && j == fruitX)
-                cout << "F";
-            else
-            {
-                bool print = false;
-                for (int k = 0; k < nTail; k++)
-                {
-                    if (tailX[k] == j && tailY[k] == i)
-                    {
-                        cout << "o";
-                        print = true;
-                    }
-                }
-                if (!print)
-                    cout << " ";
-            }
-                 
- 
-            if (j == width - 1)
-                cout << "#";
-        }
-        cout << endl;
-    }
- 
-    for (int i = 0; i < width+2; i++)
-        cout << "#";
-    cout << endl;
-    cout << "Score:" << score << endl;
+
+void Draw(){
+	system("cls"); //clear the console window
+	
+	//Set the top border of the map
+	for(int i = 0; i < (width+2); ++i){
+		cout << "#";
+	}
+	cout << endl;
+	
+	//Set the middle part of the map
+	for(int i = 0; i < height; ++i){
+		for(int j = 0; j < width; ++j){
+			if(j == 0){
+				cout << "#";
+			}
+			//Place the snake head at middle of the map
+			if(i == y && j == x){
+				cout << "O";
+			}
+			//Place the Fruit at random place of the map
+			else if(i == fruitY && j == fruitX){
+				cout << "F";
+			}
+			else{
+				bool print = false;
+				//Increase the tail length of the snake when it eats food
+				for(int k = 0; k < nTail; ++k){
+					if(xTail[k] == j && yTail[k] == i){
+						cout << "o";
+						print = true;
+					}
+				}
+				if(!print){
+					cout << " ";
+				}					
+			}					
+			
+			if(j == (width - 1)){
+				cout << "#";
+			}			
+		}
+		cout << endl;
+	}
+	
+	//Set the bottom border of the map
+	for(int  i = 0; i < (width+2); ++i){
+		cout <<"#";
+	}
+	cout << endl;	
+	cout << "Score: " << score << endl;
 }
-void Input()
-{
-    if (_kbhit())
-    {
-        switch (_getch())
-        {
-        case 'a':
-            dir = LEFT;
-            break;
-        case 'd':
-            dir = RIGHT;
-            break;
-        case 'w':
-            dir = UP;
-            break;
-        case 's':
-            dir = DOWN;
-            break;
-        case 'x':
-            gameOver = true;
-            break;
-        }
-    }
+
+void Input(){
+	//_kbhit() return a positive value if a particular key is pressed
+	if(_kbhit()){
+		//_getch() -> Return the ASCII value off the enterd charcter
+		switch(_getch()){
+			case 'a':
+				dir = LEFT;
+				break;
+			case 'd':
+				dir = RIGHT;
+				break;
+			case 'w':
+				dir = UP;
+				break;
+			case 's':
+				dir = DOWN;
+				break;
+			case 'x':
+				gameOver = true;
+				break;
+		}
+	}
 }
-void Logic()
-{
-    int prevX = tailX[0];
-    int prevY = tailY[0];
-    int prev2X, prev2Y;
-    tailX[0] = x;
-    tailY[0] = y;
-    for (int i = 1; i < nTail; i++)
-    {
-        prev2X = tailX[i];
-        prev2Y = tailY[i];
-        tailX[i] = prevX;
-        tailY[i] = prevY;
-        prevX = prev2X;
-        prevY = prev2Y;
-    }
-    switch (dir)
-    {
-    case LEFT:
-        x--;
-        break;
-    case RIGHT:
-        x++;
-        break;
-    case UP:
-        y--;
-        break;
-    case DOWN:
-        y++;
-        break;
-    default:
-        break;
-    }
-    if (x > width || x < 0 || y > height || y < 0)
-      gameOver = true;
-    if (x >= width) x = 0; else if (x < 0) x = width - 1;
-    if (y >= height) y = 0; else if (y < 0) y = height - 1;
- 
-    for (int i = 0; i < nTail; i++)
-        if (tailX[i] == x && tailY[i] == y)
-            gameOver = true;
- 
-    if (x == fruitX && y == fruitY)
-    {
-        score += 10;
-        fruitX = rand() % width;
-        fruitY = rand() % height;
-        nTail++;
-    }
+
+void Logic(){
+	
+	//prevX -> previous x coordinate of the tail
+	//prevY -> previous y coordinate of the tail
+	int prevX = xTail[0];
+	int prevY = yTail[0];
+	
+	
+	int prev2x;
+	int prev2y;
+	
+	//Set the first elemet of the xTail and yTail to follow the head
+	xTail[0] = x;
+	yTail[0] = y;
+	
+	//To move the tail
+	for(int i = 1; i < nTail; ++i){
+		prev2x = xTail[i];
+		prev2y = yTail[i];
+		xTail[i] = prevX;
+		yTail[i] = prevY;
+		prevX = prev2x;
+		prevY = prev2y;
+	}
+	
+	//To move the snake head according to keyboard command
+	switch(dir){
+		case LEFT: 
+			x--;
+			break;
+		case RIGHT:
+		 	x++;
+			break;
+		case UP:
+			y--;
+			break;
+		case DOWN:
+			y++;
+			break;
+		default:
+			break;
+	}
+	
+//	//End the game if snake head exceed the limit of the border
+//	if(x > width || x < 0 || y > height || y < 0){
+//		gameOver = true;
+//	}
+	
+	//Snake will not die if he go through the left & right border
+	if(x >= width){
+		x = 0;
+	}
+	else if(x < 0){
+		x = width - 1;
+	}
+	
+	//Snake will not die if he go through the top & bottom border
+	if(y >= height){
+		y = 0;
+	}
+	else if(y < 0){
+		y = height - 1;
+	}
+	
+	
+	//When snake hit himself the game should over
+	for(int i = 0; i < nTail; ++i){
+		if (xTail[i] == x && yTail[i] == y){
+			gameOver = true;
+			cout << "Game is over!";
+		}
+	}
+		
+	if(x == fruitX && y == fruitY){
+		//Increment the score by 10 if snake eats fruits
+		score += 10;
+		fruitX = rand() % width;
+		fruitY = rand() % height;
+		//Increment the tail when the snake eats the foods
+		nTail++;
+	}
 }
-int main()
-{
-    Setup();
-    while (!gameOver)
-    {
-        Draw();
-        Input();
-        Logic();
-        Sleep(200); 
-    }
-    return 0;
+
+int main(){
+	
+	Setup();
+	
+	while(!gameOver){
+		
+		Draw();
+		Input();
+		Logic();
+		Sleep(200);//Control the speed of the game
+	}
+	
+	return 0;
 }
